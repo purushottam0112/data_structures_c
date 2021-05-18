110870f81c9eb00202bd8444220764f853fb901f
Purushottam_kumar <purushottam01122001@gmail.com>
2021-05-18 21:37:36 +0530

    stack

---
+++ stack_in_array.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+#define MAX 100
+#define ERROR -1
+#define SUCCESS 0
+#define TRUE 1
+#define FALSE 0
+
+int push(int stack[], int ele, int *top)
+{
+    if (*top < MAX)
+    {
+        ++*top;
+        stack[(*top)] = ele;
+        printf("Sucessfully pusing\n");
+    }
+    else
+    {
+        printf("Stack overflow\n");
+    }   
+}
+
+int pop(int stack[], int *ele, int *top)
+{
+    if (*top > -1)
+    {
+        *ele = stack[*top];
+        --*top;
+    }
+    
+    else
+    {
+        printf("Stack Underflow\n");
+    }
+}
+
+int display(int stack[], int top)
+{
+    if (top == -1)
+    {
+        printf("stack is empty\n");
+    }
+    else
+    {
+        for (int i = top; i >= 0; i--)
+        {
+            printf("%d\n", stack[i]);
+        }   
+    }
+}
+
+int main()
+{
+    int stack[MAX], ele, status, top = -1, user_ch;
+    do
+    {
+        printf("Enter 1 for push\n");
+        printf("Enter 2 for pop\n");
+        printf("Enter 3 for display\n");
+        printf("Enter 0 for exit\n");
+        printf("Enter-> ");
+        scanf("%d", &user_ch);
+        if (user_ch == 1)
+        {
+            printf("Enter which element you want to insert: ");
+            scanf("%d", &ele);
+            push(stack, ele, &top);
+        }
+        else if (user_ch == 2)
+        {
+            pop(stack, &ele, &top);   
+        }
+        else if (user_ch == 3)
+        {
+            display(stack, top);
+        }  
+    } while (user_ch != 0); 
+    return 0;  
+}
+
